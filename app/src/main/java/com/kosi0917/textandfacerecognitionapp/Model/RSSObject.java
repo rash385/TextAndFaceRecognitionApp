b46baa7eb88844626fad8e52e4bab8dacd4742d1
kosi0917 <konstantin.sivolapov@netcracker.com>
2017-12-05 13:03:06 +0300

    SomeFix

---
+++ app/src/main/java/com/kosi0917/textandfacerecognitionapp/Model/RSSObject.java
@@ -0,0 +1,31 @@
+package com.kosi0917.textandfacerecognitionapp.Model;
+
+import android.os.Bundle;
+
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+
+import org.json.JSONObject;
+
+/**
+ * Created by kosi0917 on 05-Dec-17.
+ */
+
+public class RSSObject {
+    public void get_feed() {
+        GraphRequest request = GraphRequest.newMeRequest(
+                accessToken,
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(
+                            JSONObject object,
+                            GraphResponse response) {
+                        // Application code
+                    }
+                });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,name,link");
+        request.setParameters(parameters);
+        request.executeAsync();
+    }
+}
