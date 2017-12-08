b0ad909943552f45954f5bab15a43e6fdcf2ee8d
Vitalii Bokov <vitalii.bokov@netcracker.com>
2017-12-08 16:11:08 +0300

    added share button

--- app/src/main/java/com/kosi0917/textandfacerecognitionapp/Activity/VKProfileActivity.java
+++ app/src/main/java/com/kosi0917/textandfacerecognitionapp/Activity/VKProfileActivity.java
@@ -1,6 +1,7 @@
 package com.kosi0917.textandfacerecognitionapp.Activity;
 
 import android.annotation.SuppressLint;
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -19,6 +20,20 @@ import com.kosi0917.textandfacerecognitionapp.ProfileActivity;
 import com.kosi0917.textandfacerecognitionapp.R;
 import com.kosi0917.textandfacerecognitionapp.Tools.DownloadImage;
 import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKApi;
+import com.vk.sdk.api.VKApiConst;
+import com.vk.sdk.api.VKError;
+import com.vk.sdk.api.VKParameters;
+import com.vk.sdk.api.VKRequest;
+import com.vk.sdk.api.VKResponse;
+import com.vk.sdk.api.model.VKApiPhoto;
+import com.vk.sdk.api.model.VKAttachments;
+import com.vk.sdk.api.model.VKPhotoArray;
+import com.vk.sdk.api.model.VKWallPostResult;
+import com.vk.sdk.api.photo.VKImageParameters;
+import com.vk.sdk.api.photo.VKUploadImage;
+import com.vk.sdk.dialogs.VKShareDialog;
+import com.vk.sdk.dialogs.VKShareDialogBuilder;
 
 import java.io.InputStream;
 
@@ -50,10 +65,39 @@ public class VKProfileActivity extends AppCompatActivity implements View.OnClick
     @Override
     public void onClick(View view) {
         switch (view.getId()){
+            case R.id.VKShare:
+                share();
+                break;
+
             case R.id.VKLogout:
                 logout();
                 break;
         }
+    }
+
+    private void share() {
+        VKPhotoArray photos = new VKPhotoArray();
+        photos.add(new VKApiPhoto("photo-47200925_314622346"));
+        VKShareDialogBuilder builder = new VKShareDialogBuilder();
+        builder.setText("I created this post with VK Android SDK" +
+                "\nSee additional information below\n#vksdk");
+        builder.setUploadedPhotos(photos);
+        builder.setAttachmentLink("VK Android SDK information",
+                "https://vk.com/dev/android_sdk");
+        builder.setShareDialogListener(new VKShareDialog.VKShareDialogListener() {
+            @Override
+            public void onVkShareComplete(int postId) {
+                // recycle bitmap if need
+            }
+            @Override
+            public void onVkShareCancel() {
+                // recycle bitmap if need
+            }
+            @Override
+            public void onVkShareError(VKError error) {
+                // recycle bitmap if need
+            }
+        }).show(getFragmentManager(), "VK_SHARE_DIALOG");
     }
 
     private void logout(){
