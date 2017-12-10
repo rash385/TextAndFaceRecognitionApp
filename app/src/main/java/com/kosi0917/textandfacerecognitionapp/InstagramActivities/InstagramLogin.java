0f9c0046342605c5ef9f788bdfa64bd8da3dd373
sivko <sivkostja@gmail.com>
2017-12-10 22:07:01 +0300

    Added Instagram Lib

---
+++ app/src/main/java/com/kosi0917/textandfacerecognitionapp/InstagramActivities/InstagramLogin.java
@@ -0,0 +1,16 @@
+package com.kosi0917.textandfacerecognitionapp.InstagramActivities;
+
+import android.app.Activity;
+import android.widget.Button;
+
+import com.kosi0917.textandfacerecognitionapp.InstagramLib.InstagramApp;
+
+/**
+ * Created by sivko on 10.12.2017.
+ */
+
+public class InstagramLogin extends Activity {
+
+    private InstagramApp mApp;
+    private Button btnConnect, btnInfo;
+}
---
+++ app/src/main/java/com/kosi0917/textandfacerecognitionapp/InstagramLib/InstagramData.java
@@ -0,0 +1,11 @@
+package com.kosi0917.textandfacerecognitionapp.InstagramLib;
+
+/**
+ * Created by sivko on 10.12.2017.
+ */
+
+public class InstagramData {
+    public static final String CLIENT_ID = "7d285c87071643a6849a0ade4e2cd9c2";
+    public static final String CLIENT_SECRET = "fb408fb6ae1a4be3b7db7e12770b0314";
+    public static final String CALLBACK_URL = "http://127.0.0.1/instgram/connect";
+}
