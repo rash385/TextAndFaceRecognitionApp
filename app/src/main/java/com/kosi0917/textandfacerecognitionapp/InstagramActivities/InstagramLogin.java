9a0a5022423c5c01a57b05479d5cc6afd967bbd1
sivko <sivkostja@gmail.com>
2017-12-11 17:23:49 +0300

    Some design fixes

--- app/src/main/java/com/kosi0917/textandfacerecognitionapp/InstagramActivities/InstagramLogin.java
+++ app/src/main/java/com/kosi0917/textandfacerecognitionapp/InstagramActivities/InstagramLogin.java
@@ -1,6 +1,7 @@
 package com.kosi0917.textandfacerecognitionapp.InstagramActivities;
 
 import android.app.Activity;
+import android.view.View;
 import android.widget.Button;
 
 import com.kosi0917.textandfacerecognitionapp.InstagramLib.InstagramApp;
@@ -9,8 +10,13 @@ import com.kosi0917.textandfacerecognitionapp.InstagramLib.InstagramApp;
  * Created by sivko on 10.12.2017.
  */
 
-public class InstagramLogin extends Activity {
+public class InstagramLogin extends Activity implements View.OnClickListener {
 
     private InstagramApp mApp;
     private Button btnConnect, btnInfo;
+
+    @Override
+    public void onClick(View v) {
+        
+    }
 }
