3ac61636bab0ff9dcd32702c5bc57468175d8651
Vitalii Bokov <vitalii.bokov@netcracker.com>
2018-01-27 11:36:45 +0300

    new model and image

---
+++ app/src/main/java/com/kosi0917/textandfacerecognitionapp/Model/VK/Attachment.java
@@ -0,0 +1,33 @@
+
+package com.kosi0917.textandfacerecognitionapp.Model.VK;
+
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Attachment {
+
+    @SerializedName("type")
+    @Expose
+    private String type;
+    @SerializedName("photo")
+    @Expose
+    private Photo photo;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Photo getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(Photo photo) {
+        this.photo = photo;
+    }
+
+}
---
+++ app/src/main/java/com/kosi0917/textandfacerecognitionapp/Model/VK/PostSource.java
@@ -0,0 +1,21 @@
+
+package com.kosi0917.textandfacerecognitionapp.Model.VK;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class PostSource {
+
+    @SerializedName("type")
+    @Expose
+    private String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+}
